0b006dcb47d8a2b156e86884507250b81fae18b9
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2023-11-12 21:53:30 -0600

    add sources

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import argparse
 import tempfile
 import re
 
+
 class ViolationData:
   # Class to store violation data
     def __init__(self, calculate_average=True):
@@ -380,3 +381,19 @@ for id, problem_results in linter_problem_results.items():
     # Close the file
     file.close()
         
+'''
+Sources:
+ https://stackoverflow.com/questions/56875810/new-pull-request-when-one-is-already-opened
+ https://chat.openai.com/share/a12f646a-e29d-431c-8d7c-14ea042871e6
+https://chat.openai.com/share/9e7df102-375e-43b5-b78f-e893c0a8fc35
+ https://chat.openai.com/share/87f5573e-f6ee-478a-84c1-b1ed9b7f7efe
+ https://chat.openai.com/share/7fe234a8-26bb-43d3-aedb-87aadc4eeb97
+ https://chat.openai.com/share/d5be1d9c-e72f-4691-94a8-010fcf043850
+ https://chat.openai.com/share/5fd503e2-c833-4ac1-b4e1-f47b92be678d
+ https://chat.openai.com/share/56829880-fc4f-4a9d-8bda-ea17fed2087d
+ https://chat.openai.com/share/308e2057-b531-4f82-a329-2f751db0735b
+ https://chat.openai.com/share/49117405-6e08-45ec-8a25-e383880bff14
+ https://chat.openai.com/share/3bdb1313-fa4f-4071-8f66-df525420190f
+ https://chat.openai.com/share/dbe58fe2-9a0d-45b8-a1ef-1eb4edc78484
+
+'''
